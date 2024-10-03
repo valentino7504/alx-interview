973d9bae68faf13dace3cac2bf433e9e93657639
Edwin Ade <edwinmbonyjr@gmail.com>
2024-10-03 16:56:16 +0100

    perf: reduce number of computations per row by half

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -15,11 +15,16 @@ def pascal_triangle(n):
     else:
         triangle = pascal_triangle(n-1)
         last_row = triangle[-1]
-        new_row = []
-        for i in range(n):
-            if i == 0 or i == n - 1:
-                new_row.append(1)
+        new_row = [0] * n
+        even = n % 2 == 0
+        middle = int(n / 2) if even else (n // 2) + 1
+        for i in range(middle):
+            if i == 0:
+                new_row[i] = 1
             else:
-                new_row.append(last_row[i-1]+last_row[i])
+                value = last_row[i-1]+last_row[i]
+                if even or i != middle:
+                    new_row[i] = new_row[n - i - 1] = value
+        new_row[-1] = 1
         triangle.append(new_row)
         return triangle
