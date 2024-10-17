d321a34f795192aac5b4b143e8450eeab2d2169d
Edwin Ade <edwinmbonyjr@gmail.com>
2024-10-17 22:01:02 +0100

    feat: task 0 solution

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+'''
+
+Module for the minimum operations problem
+
+'''
+
+
+def minOperations(n):
+    '''
+    returns sum of prime factors
+    '''
+    if n == 0 or n == 1:
+        return 0
+    factors_sum = 0
+    while n % 2 == 0:
+        factors_sum += 2
+        n //= 2
+    for i in range(3, int(n ** 0.5), 2):
+        while n % i == 0:
+            factors_sum += i
+            n //= i
+    if n > 2:
+        factors_sum += n
+    return factors_sum
