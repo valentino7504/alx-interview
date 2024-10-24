065d14acbd5887467c1b10f2c47a8d4c2121afea
Edwin Ade <edwinmbonyjr@gmail.com>
2024-10-24 23:34:34 +0100

    feat: task solution

---
+++ 0x03-log_parsing/0-stats.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+'''
+
+0-stats.py
+
+'''
+import sys
+import re
+
+
+if __name__ == "__main__":
+    codes = ['200', '301', '400', '401', '403', '404', '405', '500']
+    code_stats = {code: 0 for code in codes}
+    reg = re.compile(r'''
+    ^
+    (\d{1,3}\.){3}\d{1,3}
+    \s-\s
+    \[\d{4}-\d{2}-\d{2}
+    \s\d{2}:\d{2}:\d{2}\.\d+\]
+    \s"GET\s\/projects\/260\s
+    HTTP\/1\.1"\s
+    (200|301|400|401|403|404|405|500)
+    \s\d+$
+    ''', re.VERBOSE)
+    line_count = 0
+    file_size = 0
+
+    def print_stats() -> None:
+        '''prints current stats'''
+        print('File size: {}'.format(file_size))
+        for code, calls in sorted(code_stats.items()):
+            if calls:
+                print('{}: {}'.format(code, calls))
+
+    try:
+        for line in sys.stdin:
+            line_count += 1
+            if reg.match(line):
+                data = line.split()
+                status_code = data[-2]
+                code_stats[status_code] += 1
+                file_size += int(data[-1])
+            if line_count % 10 == 0:
+                print_stats()
+        print_stats()
+    except KeyboardInterrupt:
+        print_stats()
+        raise
