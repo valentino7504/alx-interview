ee45ae9127eefbffb96712e9579a9cc1e5dcf6bb
Edwin Ade <edwinmbonyjr@gmail.com>
2024-11-21 19:28:05 +0100

    feat: rotate 2d matrix

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+'''
+
+Rotate 2d matrix
+
+'''
+
+
+def rotate_2d_matrix(matrix):
+    '''rotates a 2d matrix'''
+    matrix = [[matrix[j][i] for j in range(len(matrix))]
+              for i in range(len(matrix[0]))]
+    matrix = [row[::-1] for row in matrix]
