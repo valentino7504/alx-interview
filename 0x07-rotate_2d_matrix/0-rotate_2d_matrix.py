955ac0fe4eb5e6b3eb111e547ad670a46bb9e881
Edwin Ade <edwinmbonyjr@gmail.com>
2024-11-21 19:54:06 +0100

    fix: rotate matrix in place

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -8,6 +8,8 @@ Rotate 2d matrix
 
 def rotate_2d_matrix(matrix):
     '''rotates a 2d matrix'''
-    matrix = [[matrix[j][i] for j in range(len(matrix))]
-              for i in range(len(matrix[0]))]
-    matrix = [row[::-1] for row in matrix]
+    for i in range(len(matrix)):
+        for j in range(i, len(matrix)):
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+    for i in range(len(matrix)):
+        matrix[i] = matrix[i][::-1]
